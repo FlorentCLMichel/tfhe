b3a5e490338691da5a0ab301cd135c05cc486335
Florent Michel <florent.michel@optalysys.com>
2022-06-22 15:32:28 +0100

    remove build folder

--- build/libtfhe/fft_processors/optalysys_graphec/CMakeFiles/tfhe-fft-graphec.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tfhe-fft-graphec.dir/fft_processor_graphec.cpp.o"
-  "CMakeFiles/tfhe-fft-graphec.dir/lagrangehalfc_impl.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/tfhe-fft-graphec.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
